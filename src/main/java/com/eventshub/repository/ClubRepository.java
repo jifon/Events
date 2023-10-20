ff337d6a12cf8496d73b4ccbf52c9b84c666642d
ayb23nee <aygerim.budaychieva.nee@fh-zwickau.de>
2023-10-20 16:24:37 +0200

    repositories added

---
+++ src/main/java/com/eventshub/repository/ClubRepository.java
@@ -0,0 +1,8 @@
+package com.eventshub.repository;
+
+import com.eventshub.model.Club;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClubRepository extends JpaRepository<Club, Long> {
+
+}
